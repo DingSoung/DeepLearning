13ad2494547b7722acbd0645ce04bbbabb495edc
Ding Songwen <DingSoung@gmail.com>
2017-07-20 20:32:47 +0800

    重新准备

---
+++ exercises/3 y=W*x+b.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import numpy as np
+
+# 随机数据
+x_data = np.random.rand(500).astype(np.float32)
+y_data = x_data * 0.1 + 0.3
+
+# 结构 y = W * x + b
+W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.zeros([1]))
+y = x_data * W + b
+
+# 配置优化器
+loss = tf.reduce_mean(tf.square(y_data - y))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+# 初始化变量 并循环训练
+sess = tf.Session() 
+sess.run(tf.global_variables_initializer())
+for step in range(10000):
+    sess.run(train)
+    if step % 50 == 0:
+        print(step, sess.run(W), sess.run(b))
